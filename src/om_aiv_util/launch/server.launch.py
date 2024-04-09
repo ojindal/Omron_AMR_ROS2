79799fb0139ae34c3913a5f7730f07f8f75d1b10
Orish Jindal <orish.jindal.mec14@iitbhu.ac.in>
2024-04-09 15:52:44 -0700

    updated launchfile

--- src/om_aiv_util/launch/server.launch.py
+++ src/om_aiv_util/launch/server.launch.py
@@ -13,9 +13,9 @@ def generate_launch_description():
         #name='arcl_api_server',
         output='log',
         parameters=[{
-            'ip_address': "192.168.1.1",
+            'ip_address': "192.168.104.232",
             'port': 7171,
-            'def_arcl_passwd': "omron"
+            'def_arcl_passwd': "adept"
         }]
     )
 
@@ -25,22 +25,23 @@ def generate_launch_description():
         #name='ld_states_publi',
         output='screen',
         parameters=[{
-            'local_ip': "192.168.1.50",
+            'local_ip': "192.168.104.197",
             'local_port': 7179
         }]
     )
-    
+
     action_serve = Node(
         package='om_aiv_navigation',
         executable='action_server',
         #name = 'action_server',
         output='screen',
         parameters=[{
-            'ip_address': "192.168.1.1",
+            'ip_address': "192.168.104.232",
             'port': 7171,
-            'def_arcl_passwd': "omron"
+            'def_arcl_passwd': "adept"
         }]
     )
+    
     
 
     return LaunchDescription([
